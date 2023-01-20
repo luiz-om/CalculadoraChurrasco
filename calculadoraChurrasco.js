e40a28f5d262a0ab1d69b77cd0e0aa042dc3162b
Luiz-OM <luizfernando_martins@hotmail.com>
2023-01-20 14:12:21 -0400

    Finalização

--- calculadoraChurrasco.js
+++ calculadoraChurrasco.js
@@ -1,29 +1,46 @@
 const QTD_CARNE_ADULTO = 0.4;
 const QTD_CARNE_CRIANCA = 0.2;
 const QTD_BEBIDA_ADULTO = 5; //latas
-let qtdCarneAdulto = document.querySelector("#qtdCarneAdulto");
-let qtdCarneCriancas = document.querySelector("#qtdCarneCriancas");
-let radioGramas = document.querySelector(".unidade");
+let qtdCarneAdulto
+let qtdCarneCriancas
+let radioGramas = document.getElementsByName("medida");
+
 const calcular = () => {
+
   //1 ler n de adultos
   let adultos = document.querySelector("#adultos").value;
   //2 ler n de criancas
   let criancas = document.querySelector("#criancas").value;
   //3 calcular qtd carne p/adultos
-  qtdCarneAdulto.innerHTML = (+adultos * QTD_CARNE_ADULTO).toFixed(2);
+  qtdCarneAdulto = (+adultos * QTD_CARNE_ADULTO).toFixed(2);
   //4 calcular qtd carne para criancas
-  qtdCarneCriancas.innerHTML = (+criancas * 0.2).toFixed(2);
+  qtdCarneCriancas = (+criancas * 0.2).toFixed(2);
   //5 calcular qtd de bebidas p/adultos
   let qtdCerveja = QTD_BEBIDA_ADULTO * +adultos
   //mostrar resultado
-  //alert(radioGramas.checked)
-  document.getElementById("resultado").innerHTML = `
+  //alert(radioGramas.value)
+  for (var i = 0; i < radioGramas.length; i++) {
+    if (radioGramas[i].checked) {
+      if (radioGramas[i].value == 'kilogramas') {
+        document.getElementById("resultado").innerHTML = `
 
-<ul>
-<li>Quantidade de carne para adultos: ${qtdCarneAdulto.textContent} kg</li>
-<li>Quantidade de carne para criancas: ${qtdCarneCriancas.textContent} kg</li>
-<li>Quantidade de cerveja para adultos: ${qtdCerveja} latas de 350ml</li>
-</ul>
+          <ul>
+          <li>Quantidade de carne para adultos: ${qtdCarneAdulto} kg</li>
+          <li>Quantidade de carne para criancas: ${qtdCarneCriancas} kg</li>
+          <li>Quantidade de cerveja para adultos: ${qtdCerveja} latas de 350ml</li>
+          </ul>
+        `
+      } else if (radioGramas[i].value == 'gramas') {
+        document.getElementById("resultado").innerHTML = `
 
-`
+        <ul>
+        <li>Quantidade de carne para adultos: ${qtdCarneAdulto * 1000} gramas</li>
+        <li>Quantidade de carne para criancas: ${qtdCarneCriancas * 1000} gramas</li>
+        <li>Quantidade de cerveja para adultos: ${qtdCerveja} latas de 350ml</li>
+        </ul>
+      `
+      }
+
+    }
+  }
 };
